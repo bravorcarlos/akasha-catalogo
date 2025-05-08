9b6b1a38d33d103170127234ec6fbb577bcb2fc2
bravorcarlos <bravorcarlos1@gmail.com>
2025-05-07 23:15:59 -0400

    Product added to cart alert

--- src/components/product/ProductCard.vue
+++ src/components/product/ProductCard.vue
@@ -1,49 +1,48 @@
 <template>
-  <div class="col-md-3 col-6 mb-5">
-    <router-link :to="{ name: 'ProductDetail', params: { slug: product.slug } }" class="text-decoration-none">
-      <div class="card h-100">
-        <div class="image-container" :class="{ 'image-unavailable': !product.availability }">
-          <img :src="product.image" class="card-img-top" alt="Product Image"
-            style="height: 200px; object-fit: cover;" loading="lazy">
-        </div>
-        <div class="card-body d-flex flex-column">
-          <h6 class="product-name">{{ product.name.slice(0, 20) }}{{ product.name.length > 20 ? '...' : '' }}</h6>
-          <h6 class="card-subtitle mb-2 text-muted">{{ product.price }}$</h6>
-          <h6 v-if="convertedPrice">{{ convertedPrice }} {{ currencySymbol }}</h6>
-          <h6 v-else>Cargando precio...</h6>
+    <div class="col-md-3 col-6 mb-5">
+        <router-link :to="{ name: 'ProductDetail', params: { slug: product.slug } }" class="text-decoration-none">
+            <div class="card h-100">
+                <div class="image-container" :class="{ 'image-unavailable': !product.availability }">
+                    <img :src="product.image" class="card-img-top" alt="Product Image"
+                        style="height: 200px; object-fit: cover;" loading="lazy">
+                </div>
+                <div class="card-body d-flex flex-column">
+                    <h6 class="product-name">{{ product.name.slice(0, 20) }}{{ product.name.length > 20 ? '...' : '' }}
+                    </h6>
+                    <h6 class="card-subtitle mb-2 text-muted">{{ product.price }}$</h6>
+                    <h6 v-if="convertedPrice">{{ convertedPrice }} {{ currencySymbol }}</h6>
+                    <h6 v-else>Cargando precio...</h6>
 
-          <!-- Disponibilidad -->
-          <div v-if="product.availability" class="text-success mt-2">
-            Disponible
-          </div>
-          <div v-else class="text-danger mt-2">
-            No disponible
-          </div>
+                    <!-- Disponibilidad -->
+                    <div v-if="product.availability" class="text-success mt-2">
+                        Disponible
+                    </div>
+                    <div v-else class="text-danger mt-2">
+                        No disponible
+                    </div>
 
-          <!-- Botón para agregar a la bolsa -->
-          <button 
-            class="btn btn-sm btn-custom mt-3 text-white" 
-            @click.stop.prevent="handleAddToCart"
-            :disabled="!product.availability"
-          >
-            Añadir a la bolsa
-          </button>
-        </div>
-      </div>
-    </router-link>
-  </div>
+                    <!-- Botón para agregar a la bolsa -->
+                    <button class="btn btn-sm btn-custom mt-3 text-white" @click.stop.prevent="handleAddToCart"
+                        :disabled="!product.availability">
+                        Añadir a la bolsa
+                    </button>
+                </div>
+            </div>
+        </router-link>
+    </div>
 </template>
 
 <script setup>
 import { useDollarStore } from '@/stores/useExchangeRate'
 import { useCartStore } from '@/stores/cart'
 import { computed } from 'vue'
+import { useToast } from "vue-toastification"
 
 const props = defineProps({
-  product: {
-    type: Object,
-    required: true
-  }
+    product: {
+        type: Object,
+        required: true
+    }
 })
 
 const product = props.product
@@ -51,17 +50,20 @@ const dollarStore = useDollarStore()
 const cartStore = useCartStore()
 
 const convertedPrice = computed(() => {
-  return dollarStore.exchangeRate ? dollarStore.convertToLocalCurrency(product.price) : null
+    return dollarStore.exchangeRate ? dollarStore.convertToLocalCurrency(product.price) : null
 })
 
 const currencySymbol = 'Bs'
 
+const toast = useToast()
+
 const handleAddToCart = () => {
-  cartStore.addItem(product)
+    cartStore.addItem(product)
+    toast.success("¡Producto añadido a la bolsa!")
 }
 </script>
 <style scoped>
 .btn-custom {
-  background-color: #fba2c8;
+    background-color: #fba2c8;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 // import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import piniaPersist from 'pinia-plugin-persistedstate'  // Importamos el plugin de persistencia
+import piniaPersist from 'pinia-plugin-persistedstate'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
@@ -10,9 +11,16 @@ import router from './router'
 const app = createApp(App)
 
 const pinia = createPinia()
-pinia.use(piniaPersist)  // Usamos el plugin de persistencia
-
+pinia.use(piniaPersist)
 app.use(pinia)
 app.use(router)
+
+app.use(Toast, {
+    position: 'top-right',
+    timeout: 3000,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+})
 
 app.mount('#app')
